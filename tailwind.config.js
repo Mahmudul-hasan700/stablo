c2754568df26f29340f69e96bc0d9834a09a5d18
MAHMUDUL HASAN <128229930+Mahmudul-hasan700@users.noreply.github.com>
2024-01-08 17:14:43 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./lib/**/*.{js,ts,jsx,tsx}"
   ],
   darkMode: "class", // or 'media' or 'class'
   theme: {
